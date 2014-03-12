0aff4aa31129ec6851ffc1865804efe8cd1373f0
Todd Wolfson <todd@twolfson.com>
2014-03-11 23:06:43 -0700

    Completed test cases for publishing

--- test/foundry-release-pypi_test.js
+++ test/foundry-release-pypi_test.js
@@ -20,14 +20,9 @@ childProcess.exec = function () {
 };
 
 // Define our test
-var newParams = {
+var initialParams = {
   version: '0.1.0',
   message: 'Release 0.1.0',
-  description: null
-};
-var oldParams = {
-  version: '0.3.0',
-  message: 'Release 0.3.0',
   description: null
 };
 describe.skip('Setting the version', function () {
@@ -35,11 +30,7 @@ describe.skip('Setting the version', function () {
     var fixtureDir = fixtureUtils.fixtureDir('pypi');
     before(function setVersion (done) {
       this.execStub = sinon.stub(shell, 'exec');
-      pypiRelease.setVersion({
-        version: '0.1.0',
-        message: 'Release 0.1.0',
-        description: null
-      }, done);
+      pypiRelease.setVersion(initialParams, done);
     });
     after(function unstub () {
       this.execStub.restore();
@@ -53,7 +44,7 @@ describe.skip('Setting the version', function () {
   });
 });
 
-describe.only('Registering', function () {
+describe('Registering', function () {
   function register(params) {
     before(function regsiterFn (done) {
       this.execStub = sinon.stub(shell, 'exec');
@@ -67,35 +58,50 @@ describe.only('Registering', function () {
 
   describe('a new PyPI package', function () {
     var fixtureDir = fixtureUtils.fixtureDir('pypi');
-    register({
-      version: '0.1.0',
-      message: 'Release 0.1.0',
-      description: null
-    });
+    register(initialParams);
 
     it('registers the package', function () {
       expect(this.execStub.args[0]).to.deep.equal(['python setup.py register']);
     });
-
-    // it('publishes the package', function () {
-    //   expect(this.execStubPublish.args[0][0]).to.contain(['python setup.py sdist']);
-    // });
   });
 
   describe('in a private PyPI package', function () {
     var fixtureDir = fixtureUtils.fixtureDir('pypi-private');
-    register({
-      version: '0.1.0',
-      message: 'Release 0.1.0',
-      description: null
-    });
+    register(initialParams);
 
     it('does not register the package', function () {
       expect(this.execStub.args).to.have.property('length', 0);
     });
+  });
+});
 
-    // it('does not register or publish the package', function () {
-    //   expect(this.execStub.args).to.have.property('length', 0);
-    // });
+describe('Publishing', function () {
+  function publish(params) {
+    before(function publishFn (done) {
+      this.execStub = sinon.stub(shell, 'exec');
+      pypiRelease.publish(params, done);
+    });
+    after(function unstub () {
+      this.execStub.restore();
+      delete this.execStub;
+    });
+  }
+
+  describe('a new PyPI package', function () {
+    var fixtureDir = fixtureUtils.fixtureDir('pypi');
+    publish(initialParams);
+
+    it('publishes the package', function () {
+      expect(this.execStubPublish.args[0][0]).to.contain(['python setup.py sdist']);
+    });
+  });
+
+  describe('in a private PyPI package', function () {
+    var fixtureDir = fixtureUtils.fixtureDir('pypi-private');
+    publish(initialParams);
+
+    it('does not publish the package', function () {
+      expect(this.execStub.args).to.have.property('length', 0);
+    });
   });
 });
